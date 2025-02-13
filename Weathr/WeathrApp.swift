9573c5607c1d835206e92b5e1a401c94fa5ddf09
Thomas Versteeg <t.versteeg2@student.avans.nl>
2025-02-13 12:41:50 +0100

    Initial Commit

---
+++ Weathr/WeathrApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeathrApp.swift
+//  Weathr
+//
+//  Created by Thomas Versteeg on 13/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeathrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeathrTests/WeathrTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeathrTests.swift
+//  WeathrTests
+//
+//  Created by Thomas Versteeg on 13/02/2025.
+//
+
+import Testing
+@testable import Weathr
+
+struct WeathrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
