84d05eb334db4b3c68b80018a364975ed190fbc2
Thomas Versteeg <t.versteeg2@student.avans.nl>
2025-02-13 13:34:44 +0100

    Af

--- Weathr/ContentView.swift
+++ Weathr/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var weatherData: WeatherData?
+    let urlString = "https://api.openweathermap.org/data/2.5/weather?q=Oss&units=metric&appid=[APPID]"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("Lenticular_Cloud")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea(edges: .all)
+            VStack {
+                Text("Oss")
+                    .font(.custom("Helvetica Neue UltraLight", size: 60))
+                Text(getTemperatureString())
+                    .font(.custom("Helvetica Neue UltraLight", size: 90))
+            }
+            .padding()
         }
-        .padding()
+        .onAppear(perform: loadData)
+    }
+    func loadData() {
+        guard let url = URL(string: urlString) else {
+            print("ERROR: Cannot create URL")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("ERROR: (Fetch failed) \(error)")
+                return
+            }
+            guard let data = data else {
+                print("ERROR: No data returned from API call")
+                return
+            }
+            var newWeatherData: WeatherData?
+            do {
+                newWeatherData = try JSONDecoder().decode(WeatherData.self, from: data)
+            } catch let error as NSError {
+                print("ERROR: (JSON decoding failed) \(error)")
+            }
+            if newWeatherData == nil {
+                print("ERROR: Read or decoding failed for JSON data")
+            }
+            DispatchQueue.main.async {
+                self.weatherData = newWeatherData
+            }
+        }
+        task.resume()
+    }
+    
+    func getTemperatureString() -> String {
+        if let weatherData = weatherData {
+            return String(format: "%.1f°C", weatherData.main.temp)
+        } else {
+            return "?°C"
+        }
     }
 }
 
